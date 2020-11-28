d31b6ebfe0ede71d4d0065b70483fad0fa7ba8c4
Srinivas Reddy Cheruku <ssriche@amazon.com>
2020-11-28 14:48:24 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-
 from my_pipeline.my_pipeline_stack import MyPipelineStack
 
-
 app = core.App()
-MyPipelineStack(app, "my-pipeline")
+
+MyPipelineStack(app, "my-pipeline",
+    env=core.Environment(account="560360184571", region="us-east-2")
+)
 
 app.synth()
--- my_pipeline/my_pipeline_stack.py
+++ my_pipeline/my_pipeline_stack.py
@@ -1,9 +1,33 @@
-from aws_cdk import core
+from aws_cdk.core import Stack, StackProps, Construct, SecretValue
+from aws_cdk.pipelines import CdkPipeline, SimpleSynthAction
 
+import aws_cdk.aws_codepipeline as codepipeline
+import aws_cdk.aws_codepipeline_actions as codepipeline_actions
 
-class MyPipelineStack(core.Stack):
+class MyPipelineStack(Stack):
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
+
+        pipeline = CdkPipeline(self, "Pipeline",
+            pipeline_name="MyAppPipeline",
+            cloud_assembly_artifact=cloud_assembly_artifact,
+            source_action=codepipeline_actions.GitHubSourceAction(
+                action_name="GitHub",
+                output=source_artifact,
+                oauth_token=SecretValue.secrets_manager("GITHUB_TOKEN_NAME"),
+                trigger=codepipeline_actions.GitHubTrigger.POLL,
+                # Replace these with your actual GitHub project info
+                owner="srinivasreddych",
+                repo="cdk-pipelines"),
+            synth_action=SimpleSynthAction.standard_npm_synth(
+                source_artifact=source_artifact,
+                cloud_assembly_artifact=cloud_assembly_artifact,
+                # Use this if you need a build step (if you're not using ts-node
+                # or if you have TypeScript Lambdas that need to be compiled).
+                build_command="npm run build"
+            )
+        )
